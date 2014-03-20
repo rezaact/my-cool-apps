6a11b4917dbcdf326f5b6c0fa22036e4697ece6f
yudhik <brainmaster716@gmail.com>
2014-03-21 00:03:13 +0700

    Introduce Constant Class for reuseable

---
+++ src/main/java/id/co/hans/sample/constants/WsUmumUrlConstants.java
@@ -0,0 +1,15 @@
+package id.co.hans.sample.constants;
+
+/**
+ * Constant class to call ws umum rest service url
+ * 
+ * @author yudhi karunia surtan
+ * 
+ */
+public final class WsUmumUrlConstants {
+	public static final String TANGGAL_DATABASE_URL = "Ws_Umum/ambilTanggalDatabase.json?";
+	public static final String UNIT_UP_DARI_PETUGAS_URL = "Ws_Umum/ambilUnitUPdariPetugas.json?";
+	public static final String NAMA_UP_URL = "Ws_Umum/ambilNamaUp.json?";
+	public static final String KODE_PP_DARI_UNIT_UP_URL = "Ws_Umum/ambilKodePPdariUnitUP.json?unitUp=%s";
+	public static final String UNIT_UP_URL = "Ws_Umum/ambilKodePPdariUnitUP.json?";
+}
