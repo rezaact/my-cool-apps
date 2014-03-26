394eb5fd4f6dc0ffd6c820bb11ffdf7192a0064e
yudhik <brainmaster716@gmail.com>
2014-03-27 00:30:57 +0700

    complete all helper for ws umum controller

--- src/main/java/id/co/hans/sample/client/helper/WsUmumUrlHelper.java
+++ src/main/java/id/co/hans/sample/client/helper/WsUmumUrlHelper.java
@@ -93,6 +93,101 @@ public class WsUmumUrlHelper {
 			+ "ref_AmbilCabang.json";
 	private static final String REF_AMBIL_RANTING = WS_UMUM_PREFIX
 			+ "ref_AmbilRanting.json";
+	private static final String REF_AMBIL_RANTING_DENGAN_SEMUA = WS_UMUM_PREFIX
+			+ "ref_AmbilRantingDngnSemua.json";
+	private static final String REF_AMBIL_PAYMENT_POINT = WS_UMUM_PREFIX
+			+ "ref_AmbilPaymentPoint.json";
+	private static final String REF_AMBIL_PETUGAS = WS_UMUM_PREFIX
+			+ "ref_AmbilPetugas.json";
+	private static final String AMBIL_NAMA_AP_DARI_UP = WS_UMUM_PREFIX
+			+ "ambilNamaApdariUp.json";
+	private static final String AMBIL_NAMA_KD_DARI_UP = WS_UMUM_PREFIX
+			+ "ambilNamaKddariUp.json";
+	private static final String AMBIL_NAMA_KD_DARI_AP = WS_UMUM_PREFIX
+			+ "ambilNamaKddariAP.json";
+	private static final String AMBIL_NAMA_AP = WS_UMUM_PREFIX
+			+ "ambilNamaAP.json";
+	private static final String AMBIL_ALAMAT_DARI_UP = WS_UMUM_PREFIX
+			+ "ambilAlamatdariUp.json";
+	private static final String AMBIL_IDPEL_FROM_KONTRAK_KONTROL = WS_UMUM_PREFIX
+			+ "ambilIDPELfromKONTRAKKONTROLDIL.json";
+	private static final String AMBIL_PAYMENT_POINT_DARI_KODE_PP = WS_UMUM_PREFIX
+			+ "ambilNamaPaymentPointdariKodePp.json";
+	private static final String GET_PETUGAS_DARI_TABLE_USERS = WS_UMUM_PREFIX
+			+ "getPetugasDariTableUsers.json";
+	private static final String GET_PIUTANG_INFO = WS_UMUM_PREFIX
+			+ "getPiutangInfo.json";
+	private static final String GET_PELUNASAN_INDIVIDU = WS_UMUM_PREFIX
+			+ "getPelunasanIndividu.json";
+	private static final String INFORM = WS_UMUM_PREFIX + "Inform.json";
+	private static final String DS_ORACLE = WS_UMUM_PREFIX + "dsOracle.json";
+	private static final String CARI_IDPEL_INDIVIDU = WS_UMUM_PREFIX
+			+ "cariPelIndividu.json";
+	private static final String CAR_MASTER_PELANGGAN_INDIVIDU = WS_UMUM_PREFIX
+			+ "carMasterPelangganIndividu.json";
+	private static final String GET_DIL = WS_UMUM_PREFIX + "getDil.json";
+	private static final String GET_DPP_INDIVIDU = WS_UMUM_PREFIX
+			+ "getDPPIndividu.json";
+	private static final String CON_ORACLE = WS_UMUM_PREFIX + "ConnOracle.json";
+	private static final String CARI_DIL_INFO = WS_UMUM_PREFIX
+			+ "caridilinfo.json";
+	private static final String CAR_DPP_INFO = WS_UMUM_PREFIX
+			+ "carDPPInfo.json";
+	private static final String GET_KODE_SIKLIS = WS_UMUM_PREFIX
+			+ "ambilKodesiklis.json";
+	private static final String AMBIL_PEJABAT_ID01 = WS_UMUM_PREFIX
+			+ "ambilPejabatID01.json";
+	private static final String AMBIL_PEJABAT_ID02 = WS_UMUM_PREFIX
+			+ "ambilPejabatID02.json";
+	private static final String CETAK_INFO = WS_UMUM_PREFIX + "cetakInfo.json";
+	private static final String CETAK_INVOICE = WS_UMUM_PREFIX
+			+ "cetakInvoice.json";
+	private static final String GET_MASTER_KOLEKTIF = WS_UMUM_PREFIX
+			+ "getMasterKolektif.json";
+	private static final String LIHAT_HISTORY_TRANSAKSI = WS_UMUM_PREFIX
+			+ "LihatHistoTrans.json";
+
+	public static String doCetakInfo(String idPelanggan, String blnAwal,
+			String blnAkhir) {
+		UrlBuilder builder = new UrlBuilder();
+		builder.setPath(CETAK_INFO);
+		builder.setParameter("IdPel", idPelanggan);
+		builder.setParameter("BlnAwal", blnAwal);
+		builder.setParameter("BlnAkhir", blnAkhir);
+		return builder.toString();
+	}
+
+	public static String doCetakInvoice(String idPelanggan, String blnAwal,
+			String blnAkhir) {
+		UrlBuilder builder = new UrlBuilder();
+		builder.setPath(CETAK_INVOICE);
+		builder.setParameter("IdPel", idPelanggan);
+		builder.setParameter("BlnAwal", blnAwal);
+		builder.setParameter("BlnAkhir", blnAkhir);
+		return builder.toString();
+	}
+
+	public static String doDsOracle(String mSql, String dsName) {
+		UrlBuilder builder = new UrlBuilder();
+		builder.setPath(DS_ORACLE);
+		builder.setParameter("mSql", mSql);
+		builder.setParameter("dsName", dsName);
+		return builder.toString();
+	}
+
+	public static String doInform(String idPelanggan) {
+		UrlBuilder builder = new UrlBuilder();
+		builder.setPath(INFORM);
+		builder.setParameter("idpel", idPelanggan);
+		return builder.toString();
+	}
+
+	public static String getAlamatDariUp(String unitUp) {
+		UrlBuilder builder = new UrlBuilder();
+		builder.setPath(AMBIL_ALAMAT_DARI_UP);
+		builder.setParameter("unitUp", unitUp);
+		return builder.toString();
+	}
 
 	public static String getBulanMinusSatuURL(String thbl) {
 		UrlBuilder builder = new UrlBuilder();
@@ -105,6 +200,13 @@ public class WsUmumUrlHelper {
 		UrlBuilder builder = new UrlBuilder();
 		builder.setParameter("thbl", thbl);
 		builder.setPath(AMBIL_BULAN_TAMBAH_SATU_URL);
+		return builder.toString();
+	}
+
+	public static String getCarDPPInfo(String clsdpp) {
+		UrlBuilder builder = new UrlBuilder();
+		builder.setPath(CAR_DPP_INFO);
+		builder.setParameter("clsdpp", clsdpp);
 		return builder.toString();
 	}
 
@@ -184,6 +286,19 @@ public class WsUmumUrlHelper {
 		return builder.toString();
 	}
 
+	public static String getCarMasterPelangganIndividu(String clsPel) {
+		UrlBuilder builder = new UrlBuilder();
+		builder.setPath(CAR_MASTER_PELANGGAN_INDIVIDU);
+		builder.setParameter("clsPel", clsPel);
+		return builder.toString();
+	}
+
+	public static String getConnOracle() {
+		UrlBuilder builder = new UrlBuilder();
+		builder.setPath(CON_ORACLE);
+		return builder.toString();
+	}
+
 	public static String getCreateTanggalAgendaURL(String ikdupnumerik) {
 		UrlBuilder builder = new UrlBuilder();
 		builder.setParameter("ikdupnumerik", ikdupnumerik);
@@ -203,10 +318,55 @@ public class WsUmumUrlHelper {
 		return builder.toString();
 	}
 
+	public static String getDIL(String clsdpp) {
+		UrlBuilder builder = new UrlBuilder();
+		builder.setPath(GET_DIL);
+		builder.setParameter("clsdpp", clsdpp);
+		return builder.toString();
+	}
+
+	public static String getDILInfo(String clsPel) {
+		UrlBuilder builder = new UrlBuilder();
+		builder.setPath(CARI_DIL_INFO);
+		builder.setParameter("clsPel", clsPel);
+		return builder.toString();
+	}
+
 	public static String getDILPelangganURL(String idPelanggan) {
 		UrlBuilder builder = new UrlBuilder();
 		builder.setParameter("idpel", idPelanggan);
 		builder.setPath(AMBIL_DIL_PELANGGAN_URL);
+		return builder.toString();
+	}
+
+	public static String getDPPIndividu(String clsdpp) {
+		UrlBuilder builder = new UrlBuilder();
+		builder.setPath(GET_DPP_INDIVIDU);
+		builder.setParameter("clsdpp", clsdpp);
+		return builder.toString();
+	}
+
+	public static String getHistoryTransaksi(String idPelanggan, String blth) {
+		UrlBuilder builder = new UrlBuilder();
+		builder.setPath(LIHAT_HISTORY_TRANSAKSI);
+		builder.setParameter("IdPel", idPelanggan);
+		builder.setParameter("BLTH", blth);
+		return builder.toString();
+	}
+
+	public static String getIdpelangganFromKontrakKontrol(String kontrak,
+			String kontrol) {
+		UrlBuilder builder = new UrlBuilder();
+		builder.setPath(AMBIL_IDPEL_FROM_KONTRAK_KONTROL);
+		builder.setParameter("kontrak", kontrak);
+		builder.setParameter("kontrol", kontrol);
+		return builder.toString();
+	}
+
+	public static String getIdpelIndividu(String clsPel) {
+		UrlBuilder builder = new UrlBuilder();
+		builder.setPath(CARI_IDPEL_INDIVIDU);
+		builder.setParameter("clsPel", clsPel);
 		return builder.toString();
 	}
 
@@ -232,6 +392,13 @@ public class WsUmumUrlHelper {
 		UrlBuilder builder = new UrlBuilder();
 		builder.setParameter("unitUp", unitUp);
 		builder.setPath(KODE_PP_DARI_UNIT_UP_URL);
+		return builder.toString();
+	}
+
+	public static String getKodeSiklis(String unitUp) {
+		UrlBuilder builder = new UrlBuilder();
+		builder.setPath(GET_KODE_SIKLIS);
+		builder.setParameter("unitUp", unitUp);
 		return builder.toString();
 	}
 
@@ -261,15 +428,48 @@ public class WsUmumUrlHelper {
 		return builder.toString();
 	}
 
+	public static String getMasterKolektif() {
+		UrlBuilder builder = new UrlBuilder();
+		builder.setPath(GET_MASTER_KOLEKTIF);
+		return builder.toString();
+	}
+
 	public static String getNamaAlamatDefaultURL() {
 		UrlBuilder builder = new UrlBuilder();
 		builder.setPath(AMBIL_ALAMAT_DEFAULT);
 		return builder.toString();
 	}
 
+	public static String getNamaAp() {
+		UrlBuilder builder = new UrlBuilder();
+		builder.setPath(AMBIL_NAMA_AP);
+		return builder.toString();
+	}
+
+	public static String getNamaApDariUp(String unitUp) {
+		UrlBuilder builder = new UrlBuilder();
+		builder.setParameter("unitUp", unitUp);
+		builder.setPath(AMBIL_NAMA_AP_DARI_UP);
+		return builder.toString();
+	}
+
 	public static String getNamaCabangDefaultURL() {
 		UrlBuilder builder = new UrlBuilder();
 		builder.setPath(AMBIL_NAMA_CABANG_DEFAULT);
+		return builder.toString();
+	}
+
+	public static String getNamaKDDariAp(String unitAp) {
+		UrlBuilder builder = new UrlBuilder();
+		builder.setParameter("unitAp", unitAp);
+		builder.setPath(AMBIL_NAMA_KD_DARI_AP);
+		return builder.toString();
+	}
+
+	public static String getNamaKDDariUp(String unitUp) {
+		UrlBuilder builder = new UrlBuilder();
+		builder.setParameter("unitUp", unitUp);
+		builder.setPath(AMBIL_NAMA_KD_DARI_UP);
 		return builder.toString();
 	}
 
@@ -305,6 +505,49 @@ public class WsUmumUrlHelper {
 		return builder.toString();
 	}
 
+	public static String getPaymentPointDariKodePP(String kodepp) {
+		UrlBuilder builder = new UrlBuilder();
+		builder.setPath(AMBIL_PAYMENT_POINT_DARI_KODE_PP);
+		builder.setParameter("kodepp", kodepp);
+		return builder.toString();
+	}
+
+	public static String getPejabatId01(String unitUp) {
+		UrlBuilder builder = new UrlBuilder();
+		builder.setPath(AMBIL_PEJABAT_ID01);
+		builder.setParameter("unitUp", unitUp);
+		return builder.toString();
+	}
+
+	public static String getPejabatId02(String unitUp) {
+		UrlBuilder builder = new UrlBuilder();
+		builder.setPath(AMBIL_PEJABAT_ID02);
+		builder.setParameter("unitUp", unitUp);
+		return builder.toString();
+	}
+
+	public static String getPelunasaIndividu(String jenisPiutang, String clsAR) {
+		UrlBuilder builder = new UrlBuilder();
+		builder.setPath(GET_PELUNASAN_INDIVIDU);
+		builder.setParameter("JENISPIUTANG", jenisPiutang);
+		builder.setParameter("clsAR", clsAR);
+		return builder.toString();
+	}
+
+	public static String getPetugasDariTableUsers(String strData) {
+		UrlBuilder builder = new UrlBuilder();
+		builder.setPath(GET_PETUGAS_DARI_TABLE_USERS);
+		builder.setParameter("strData", strData);
+		return builder.toString();
+	}
+
+	public static String getPiutangInfo(String clsAR) {
+		UrlBuilder builder = new UrlBuilder();
+		builder.setPath(GET_PIUTANG_INFO);
+		builder.setParameter("clsAR", clsAR);
+		return builder.toString();
+	}
+
 	public static String getREFAmbilCabangURL(String induk) {
 		UrlBuilder builder = new UrlBuilder();
 		builder.setPath(REF_AMBIL_CABANG);
@@ -318,9 +561,30 @@ public class WsUmumUrlHelper {
 		return builder.toString();
 	}
 
+	public static String getREFAmbilPaymentPoint(String ranting) {
+		UrlBuilder builder = new UrlBuilder();
+		builder.setParameter("ranting", ranting);
+		builder.setPath(REF_AMBIL_PAYMENT_POINT);
+		return builder.toString();
+	}
+
+	public static String getREFAmbilPetugas(String pp) {
+		UrlBuilder builder = new UrlBuilder();
+		builder.setParameter("pp", pp);
+		builder.setPath(REF_AMBIL_PETUGAS);
+		return builder.toString();
+	}
+
 	public static String getREFAmbilRanting(String cabang) {
 		UrlBuilder builder = new UrlBuilder();
 		builder.setPath(REF_AMBIL_RANTING);
+		builder.setParameter("cabang", cabang);
+		return builder.toString();
+	}
+
+	public static String getREFAmbilRantingDenganSemua(String cabang) {
+		UrlBuilder builder = new UrlBuilder();
+		builder.setPath(REF_AMBIL_RANTING_DENGAN_SEMUA);
 		builder.setParameter("cabang", cabang);
 		return builder.toString();
 	}
