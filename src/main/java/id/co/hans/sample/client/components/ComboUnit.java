d4d6f486f4e113ddc83e506c6e74539c11a040ad
yudhik <brainmaster716@gmail.com>
2014-04-06 16:55:15 +0700

    synchronize ComboUnit Component

--- src/main/java/id/co/hans/sample/client/components/ComboUnit.java
+++ src/main/java/id/co/hans/sample/client/components/ComboUnit.java
@@ -1,7 +1,5 @@
 package id.co.hans.sample.client.components;
 
-import id.co.hans.sample.client.helper.WsUmumUrlHelper;
-
 import java.util.Map;
 
 import com.google.gwt.event.logical.shared.SelectionEvent;
@@ -14,59 +12,101 @@ import com.sencha.gxt.widget.core.client.form.TextField;
 
 public class ComboUnit extends AbstractComboComponent {
 
-	@SuppressWarnings("unused")
-	private static final String DUMMY_URL = "BasicProject/thuGetComboData.json";
-	
-	private HorizontalPanel hp;
-	private IconComboBox cbUnitUp;
-	private TextField tfDescription;
-	private String selectedValue;
+  @SuppressWarnings("unused")
+  private static final String DUMMY_URL = "BasicProject/thuGetComboData.json";
 
-	@Override
-	public Widget asWidget() {
-		cbUnitUp = new IconComboBox();
-		cbUnitUp.setStoreUrl(WsUmumUrlHelper.getUnitUpURL());
-		cbUnitUp.setComboWidth(200);
-		cbUnitUp.setLabelWidth(180);
+  private HorizontalPanel hp;
+  private IconComboBox cbUnitUp;
+  private TextField tfDescription;
+  private String selectedValue;
+  private Integer addUpSemua = 0;
+  private String levelUnits = "";
+  private String userUnit = "";
 
-		cbUnitUp.addSelectionHandler(new SelectionHandler<Map<String, String>>() {
-			@Override
-			public void onSelection(SelectionEvent<Map<String, String>> event) {
-				Map<String, String> data = (Map<String, String>) event
-						.getSelectedItem();
+  public ComboUnit() {}
 
-				tfDescription.setText(data.get("fieldValue"));
-				selectedValue = data.get("fieldValue");
-				
-				onComboChange(data.get("fieldValue"));
-			}
-		});
+  public ComboUnit(String levelUnits, String userUnits) {
+    this.levelUnits = levelUnits;
+    this.userUnit = userUnits;
+  }
 
-		tfDescription = new TextField();
+  public ComboUnit(String levelUnits, String userUnits, Integer addUpSemua) {
+    this.levelUnits = levelUnits;
+    this.userUnit = userUnits;
+    this.addUpSemua = addUpSemua;
+  }
 
-		Label spacer1 = new Label("");
-		spacer1.setPixelSize(10, 1);
+  @Override
+  public Widget asWidget() {
+    cbUnitUp = new IconComboBox();
+    cbUnitUp.setStoreUrl("components/getComboUnitPelayanan.json?levelUnits=" + levelUnits
+        + "&userUnit=" + userUnit + "&addUpSemua=" + addUpSemua + "&selectedUnitAp=" + userUnit);
+    cbUnitUp.setComboWidth(200);
+    cbUnitUp.setLabelWidth(180);
 
-		hp = new HorizontalPanel();
-		hp.add(cbUnitUp);
-		hp.add(spacer1);
-		hp.add(tfDescription);
+    cbUnitUp.addSelectionHandler(new SelectionHandler<Map<String, String>>() {
+      @Override
+      public void onSelection(SelectionEvent<Map<String, String>> event) {
+        Map<String, String> data = event.getSelectedItem();
 
-		cbUnitUp.loadStore();
+        tfDescription.setText(data.get("fieldValue"));
+        selectedValue = data.get("fieldValue");
 
-		return new FieldLabel(hp, "Unit");
-	}
+        onComboChange(data.get("fieldValue"));
+      }
+    });
 
-	public String getSelectedValue() {
-		return this.selectedValue;
-	}
+    tfDescription = new TextField();
 
-	public IconComboBox getCbUnitUp() {
-		return this.cbUnitUp;
-	}
+    Label spacer1 = new Label("");
+    spacer1.setPixelSize(10, 1);
 
-	public TextField getCbUnitUpDesc() {
-		return this.tfDescription;
-	}
-	
+    hp = new HorizontalPanel();
+    hp.add(cbUnitUp);
+    hp.add(spacer1);
+    hp.add(tfDescription);
+
+    cbUnitUp.loadStore();
+
+    return new FieldLabel(hp, "Unit");
+  }
+
+  public Integer getAddUpSemua() {
+    return addUpSemua;
+  }
+
+  public IconComboBox getCbUnitUp() {
+    return this.cbUnitUp;
+  }
+
+  public TextField getCbUnitUpDesc() {
+    return this.tfDescription;
+  }
+
+  public String getLevelUnits() {
+    return levelUnits;
+  }
+
+  public String getSelectedValue() {
+    return this.selectedValue;
+  }
+
+  public String getUserUnit() {
+    return userUnit;
+  }
+
+  public void setAddUpSemua(Integer addUpSemua) {
+    this.addUpSemua = addUpSemua;
+  }
+
+  public void setLevelUnits(String levelUnits) {
+    this.levelUnits = levelUnits;
+  }
+
+  public void setUserUnit(String userUnit) {
+    this.userUnit = userUnit;
+  }
+
+
+
 }
